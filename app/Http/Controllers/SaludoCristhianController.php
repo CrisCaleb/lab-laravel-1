f97e7e69c4bd560330765420d5f92020d3f7f112
Criscaleb <cristhianc@outlook.com>
2025-09-09 20:05:54 -0400

    modifique

---
+++ app/Http/Controllers/SaludoCristhianController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SaludoCristhianController extends Controller
+{
+    //
+    public function index()
+{
+    return "Hola soy Cristhian Cepeda";
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\SaludoController;
+Route::get('/saludo', [SaludoController::class, 'index']);
+
+use App\Http\Controllers\SaludoCristhianController;
+Route::get('/saludoCristhian', [SaludoCristhianController::class, 'index']);
